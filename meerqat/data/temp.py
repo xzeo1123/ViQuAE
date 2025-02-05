235a96c31ba9a69bc1e6a11da3ef58c98a064b04
Đinh Phi Hùng <114269957+xzeo1123@users.noreply.github.com>
2025-02-06 06:44:53 +0700

    Update temp.py

--- meerqat/data/temp.py
+++ meerqat/data/temp.py
@@ -15,34 +15,28 @@ kb['humans_with_faces'] = kb['humans_with_faces'].map(lambda item: {'is_human': 
 kb['humans_without_faces'] = kb['humans_without_faces'].map(lambda item: {'is_human': True})
 kb['non_humans'] = kb['non_humans'].map(lambda item: {'is_human': False})
 
-# Kết hợp lại
+# Lưu toàn bộ dữ liệu (100%) vào file
 kb_recat = concatenate_datasets([kb['non_humans'], kb['humans_with_faces'], kb['humans_without_faces']])
-
-# Lưu ra file
 kb_recat.save_to_disk('data/viquae_wikipedia_recat')
 
-
 # Lấy 50% dữ liệu của mỗi phần
-kb['humans_with_faces'] = kb['humans_with_faces'].train_test_split(test_size=0.5)['test']
-kb['humans_without_faces'] = kb['humans_without_faces'].train_test_split(test_size=0.5)['test']
-kb['non_humans'] = kb['non_humans'].train_test_split(test_size=0.5)['test']
+kb_50percent = {
+    'humans_with_faces': kb['humans_with_faces'].train_test_split(test_size=0.5)['test'],
+    'humans_without_faces': kb['humans_without_faces'].train_test_split(test_size=0.5)['test'],
+    'non_humans': kb['non_humans'].train_test_split(test_size=0.5)['test']
+}
 
-# Kết hợp lại
-kb_recat = concatenate_datasets([kb['non_humans'], kb['humans_with_faces'], kb['humans_without_faces']])
+# Kết hợp lại và lưu 50%
+kb_recat_50percent = concatenate_datasets([kb_50percent['non_humans'], kb_50percent['humans_with_faces'], kb_50percent['humans_without_faces']])
+kb_recat_50percent.save_to_disk('data/viquae_wikipedia_recat_50percent')
 
-# Lưu ra file
-kb_recat.save_to_disk('data/viquae_wikipedia_recat_50percent')
+# Lấy 10% dữ liệu của mỗi phần từ gốc (100%)
+kb_10percent = {
+    'humans_with_faces': kb['humans_with_faces'].train_test_split(test_size=0.1)['test'],
+    'humans_without_faces': kb['humans_without_faces'].train_test_split(test_size=0.1)['test'],
+    'non_humans': kb['non_humans'].train_test_split(test_size=0.1)['test']
+}
 
-
-# Lấy 10% dữ liệu của mỗi phần
-kb['humans_with_faces'] = kb['humans_with_faces'].train_test_split(test_size=0.1)['test']
-kb['humans_without_faces'] = kb['humans_without_faces'].train_test_split(test_size=0.1)['test']
-kb['non_humans'] = kb['non_humans'].train_test_split(test_size=0.1)['test']
-
-# Kết hợp lại
-kb_recat = concatenate_datasets([kb['non_humans'], kb['humans_with_faces'], kb['humans_without_faces']])
-
-# Lưu ra file
-kb_recat.save_to_disk('data/viquae_wikipedia_recat_10percent')
-
-
+# Kết hợp lại và lưu 10%
+kb_recat_10percent = concatenate_datasets([kb_10percent['non_humans'], kb_10percent['humans_with_faces'], kb_10percent['humans_without_faces']])
+kb_recat_10percent.save_to_disk('data/viquae_wikipedia_recat_10percent')
